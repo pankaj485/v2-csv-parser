0c6156615f3b795ab85288a3a757890e30beb124
pankaj485 <pankajsamar2@gmail.com>
2023-08-09 22:26:23 +0530

    implement directory checking and getting valid data

---
+++ src/utils/handleFiles.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import handleError from "./handleError";
+
+const checkUploadDir = (baseUploadPath: string) => {
+	try {
+		if (!fs.existsSync(baseUploadPath)) {
+			console.log("Creating upload directory.");
+			fs.mkdirSync(baseUploadPath, { recursive: true });
+		} else {
+			console.log("Upload directory exists");
+		}
+	} catch (error) {
+		handleError(error, "Error while checking file upload directory");
+	}
+};
+
+const getValidFiles = (baseUploadPath: string) => {
+	try {
+		const files = fs.readdirSync(baseUploadPath);
+
+		// only return .csv format files
+		return files.filter((currentFile) => {
+			const _ = currentFile.split(".");
+			return _[_.length - 1] === "csv";
+		});
+	} catch (error) {
+		handleError(error, "error while getting valid files");
+	}
+};
+
+export { checkUploadDir, getValidFiles };
