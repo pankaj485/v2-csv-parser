bf399836b0212f4685507b79e25cce7b3f1e3950
pankaj485 <pankajsamar2@gmail.com>
2023-08-23 23:30:04 +0530

    implement separate parser

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,9 @@
 import path from "path";
-
-import { checkUploadDir, getValidFiles } from "./utils/handleFiles";
-import getFileHeaders from "./getFileHeaders";
-import { getCsvData } from "./getCsvData";
-import { getFiles } from "./utils/getFiles";
+import { processData } from "./parser";
 
 const baseUploadPath = path.resolve(__dirname, "../", "public", "uploads");
 const headers = ["industry", "description", "level", "insta", "mojo", "vendix"];
 
-async function processData() {
-	const isValidUpoadDir = checkUploadDir(baseUploadPath);
-	try {
-		if (isValidUpoadDir) {
-			const headerData: any = await getFileHeaders(baseUploadPath, {
-				headerColumn: 1,
-				headers: headers,
-			});
-
-			console.log("\nHeaders data: ", headerData);
-
-			const fileFullPath = path.resolve(__dirname, baseUploadPath, getFiles(baseUploadPath)[0]);
-			const { validHeaders } = headerData;
-			const parsingConfig = {
-				headers: validHeaders,
-				from_line: 1,
-				to_line: 30,
-			};
-
-			const data = await getCsvData(fileFullPath, parsingConfig);
-			console.log("\nParsed data: ", data);
-		}
-	} catch (error) {
-		console.error("An error occurred:", error);
-	}
-}
-
-processData();
+processData(baseUploadPath, headers).then((data) => {
+	console.log(data);
+});
---
+++ src/parser.ts
@@ -0,0 +1,36 @@
+import path from "path";
+
+import { checkUploadDir } from "./utils/handleFiles";
+import getFileHeaders from "./getFileHeaders";
+import { getCsvData } from "./getCsvData";
+import { getFiles } from "./utils/getFiles";
+
+async function processData(baseUploadPath: string, headers: string[]) {
+	const isValidUpoadDir = checkUploadDir(baseUploadPath);
+	try {
+		if (isValidUpoadDir) {
+			const headerData: any = await getFileHeaders(baseUploadPath, {
+				headerColumn: 1,
+				headers: headers,
+			});
+
+			// console.log("\nHeaders data: ", headerData);
+
+			const fileFullPath = path.resolve(__dirname, baseUploadPath, getFiles(baseUploadPath)[0]);
+			const { validHeaders } = headerData;
+			const parsingConfig = {
+				headers: validHeaders,
+				from_line: 1,
+				to_line: 30,
+			};
+
+			const data = await getCsvData(fileFullPath, parsingConfig);
+			// console.log("\nParsed data: ", data);
+			return data;
+		}
+	} catch (error) {
+		console.error("An error occurred:", error);
+	}
+}
+
+export { processData };
